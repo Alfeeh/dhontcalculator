b62898583602f8027ea953daae63dc8db44937b8
Josh <me@joshl.io>
2021-03-01 16:02:39 +0000

    Fixed line reading inconsistencies, added comments

--- Extractor.cs
+++ Extractor.cs
@@ -7,22 +7,25 @@ namespace VotingCalculator
 {
     class Extractor
     {
-        public List<Party> Extract(string textFile)
+        // Extracts information from the supplied text file and uses it to return a list of Party objects, 
+        // each element containing all the relevant information to each party, and other important information.
+        public Tuple<string, List<Party>, int, int> Extract(System.IO.StreamReader file)
         {
             string line;
             List<string> lines = new List<string>();
-            System.IO.StreamReader file =
-                new System.IO.StreamReader(@"Assessment1Data.txt");
 
-            while ((line = file.ReadLine()) != null)
+            while ((line = file.ReadLine()) != null) // Prevents the program from crashing when the end of the file is reached.
             {
                 lines.Add(line);
             }
 
+            // Extracts the constituency name, total seats and total votes from the first 3 lines.
+            string constituencyName = lines[0];
             int totalSeats = Int32.Parse(lines[1]);
             int totalVotes = Int32.Parse(lines[2]);
             List<Party> partyList = new List<Party>();
 
+            // Extracts the party name, original number votes and names of the seats for each party found in the text file.
             for (int i = 3; i < lines.Count; i++)
             {
                 var lineList = lines[i].Substring(0, lines[i].Length - 1).Split(",").ToList();
@@ -30,11 +33,11 @@ namespace VotingCalculator
                 int partyVotes = Int32.Parse(lineList[1]);
 
                 List<string> partySeats = lineList.GetRange(2, lineList.Count - 2);
-                Party partyObject = new Party(partyName, partyVotes, partySeats);
+                Party partyObject = new Party(partyName, partyVotes, partySeats); // Instantiates each party object.
                 partyList.Add(partyObject);
             }
-            
-            return partyList;
+
+            return new Tuple<string, List<Party>, int, int>(constituencyName, partyList, totalSeats, totalVotes);
         }
     }
 }
