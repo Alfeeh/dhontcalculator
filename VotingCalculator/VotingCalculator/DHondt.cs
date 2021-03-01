216dffac4590b8e958ca9dee993a2ec9426cc9de
JoshSCF <me@joshl.io>
2021-03-01 16:45:03 +0000

    modified:   VotingCalculator/VotingCalculator/DHondt.cs

--- VotingCalculator/VotingCalculator/DHondt.cs
+++ VotingCalculator/VotingCalculator/DHondt.cs
@@ -1,23 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using System.IO;
 
 namespace VotingCalculator
 {
     class DHondt
     {
-        // int amount of seats
-        //for (int x = 0, int y = 5; x < y; x++ )
-        //{
-        //new votes =  votes /(1 + seats)
-        // seats = seats + 1 
-        //}
-
-
-        public void Calculate(Extractor data, string textFile)
+        // the DHondt methods is used to calculate (number of sets / by the origanal votes)
+        public Tuple<string, List<Party>> Calculate(Extractor data, System.IO.StreamReader textFile)
         {
-            List<Party> partyList = data.Extract(textFile);
-            Console.WriteLine(partyList);
+            var (constituencyName, partyList, totalSeats, totalVotes) = data.Extract(textFile);
+            
+            // for the amount of total seats, divide current votes by original +1 and sort list for next iteration
+            for (int i = 0; i < totalSeats; i++)
+            {
+                partyList.Sort((x, y) => y.currentVotes.CompareTo(x.currentVotes));
+                partyList[0].amountOfSeats += 1;
+                partyList[0].currentVotes = partyList[0].originalVotes / (partyList[0].amountOfSeats + 1);
+            }
+
+            return new Tuple<string, List<Party>>(constituencyName, partyList);
+
         }
+
+        // outputs the names of the elected seats to the console (sorted)
+        public void OutputToConsole(string constituencyName, List<Party> electorals)
+        {
+            // output constituency name, iterates through each seat
+            Console.WriteLine(constituencyName);
+            for (int i = 0; i < electorals.Count; i++) 
+            {
+                // if seats exist, output a seat name for each existing seat
+                if (electorals[i].amountOfSeats > 0)
+                {
+                    Console.Write($"{electorals[i].name}");
+                    for(int j = 0; j < electorals[i].amountOfSeats; j++)
+                    {
+                        Console.Write($",{electorals[i].namesOfSeats[j]}");
+                    }
+                    Console.WriteLine(";");
+
+                }
+            }
+           
+        }
+
     }
+
+
 }
